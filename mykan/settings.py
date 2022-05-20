9f4033e922a1539dc08ff93808d14a9015ecd924
Nsiah Moses <mosay360@gmail.com>
2022-05-20 05:14:20 +0000

    static

--- mykan/settings.py
+++ mykan/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 #STATICFILES_DIRS = (
    # os.path.join(BASE_DIR, 'static'),
 #)
